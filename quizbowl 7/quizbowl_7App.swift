5101860873094200747a10508340afabc733ccd4
Jakub <Wilk>
2023-01-09 09:50:49 -0600

    Initial Commit

---
+++ quizbowl 7/quizbowl_7App.swift
@@ -0,0 +1,17 @@
+//
+//  quizbowl_7App.swift
+//  quizbowl 7
+//
+//  Created by Jakub Wilk on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct quizbowl_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
