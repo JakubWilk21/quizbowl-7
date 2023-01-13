884870461e2832f761cd2da6cab5eb29be8716e1
Jakub <Wilk>
2023-01-13 09:37:41 -0600

    created initial view

--- quizbowl 7/Views/ContentView.swift
+++ quizbowl 7/Views/ContentView.swift
@@ -8,17 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var quizManager = QuizManager()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 40) {
+                VStack(spacing: 20) {
+                    Text("Country Flag Game")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.yellow)
+                    Text("ready to test your skillz?")
+                        .foregroundColor(.yellow)
+                }
+                NavigationLink {
+                    QuestionView()
+                        .environmentObject(quizManager)
+                }
+            label: {
+                CustomButton(text: "Start")
+            }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.all)
+            .background(.cyan)
         }
-        .padding()
     }
 }
-
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
